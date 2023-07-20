572819f5dbcee40a988511c9fa3e92c3d95c3a6e
ADITYA PAREEK <80575917+Aditya01237@users.noreply.github.com>
2023-07-21 00:32:04 +0530

    files updated

--- src/components/SocialLinks.jsx
+++ src/components/SocialLinks.jsx
@@ -40,7 +40,7 @@ const SocialLinks = () => {
           Resume <BsFillPersonLinesFill size={30} />
         </>
       ),
-      href: "/Resume.docx",
+      href: "/resume.pdf",
       style: "rounded-br-md",
       download: true,
     },
